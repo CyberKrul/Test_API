1aeb68ee4faf975e691a3cf172ad36338d152c5c
Cyberkrul <etienneferns@gmail.com>
2025-09-25 17:46:16 +0530

    MODIFIED = | handler: added function 2 and 3 of Three to handle all section of the API. Pre-gemini revision version \ go.mod: ran go mod tidy \ go.sum: generated by go mod tidy |

---
+++ handler/handler.go
@@ -0,0 +1,111 @@
+package handler
+
+// this package will handle all things HTTP, and acts as the highest, front facing interface
+
+import (
+	"TAPI/service"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ==================================================
+// SERVICE STRUCT TYPES
+// ==================================================
+// RegisterDeviceRequest defines the shape of the JSON body for a device registration request.
+type RegisterDeviceRequest struct {
+	SNo             int `json:"sno"`
+	FirmwareVersion int `json:"firmwareVersion"`
+}
+
+// UpdateMeshRequest defines the shape of the JSON body for a mesh update request
+type UpdateMeshRequest struct {
+	SNo int `json:"sno"`
+}
+
+// RetrieveDeviceRequest  defines the shape of the JSON body for a device retrieval request
+type RetrieveDeviceRequest struct {
+	SNo int `json:"sno"`
+}
+
+// ==================================================
+
+// ServiceContractInstance consumes the service contract between the handler and service layer
+type ServiceContractInstance struct {
+	sci service.ServiceContractDefinition
+}
+
+// NewServiceContractInstance creates a new instance of ServiceContractInstance
+func NewServiceContractInstance(sci service.ServiceContractDefinition) *ServiceContractInstance {
+	return &ServiceContractInstance{
+		sci: sci,
+	}
+}
+
+// HandleCreateDeviceRequest will handle the HTTP request to add a new device to the DB
+func (s *ServiceContractInstance) HandleCreateDeviceRequest(c *gin.Context) {
+	// instantiate a new object to bind the HTTP JSON to
+	var newDevice RegisterDeviceRequest
+
+	// Gin will handle decoding and error response
+	if err := c.ShouldBindJSON(&newDevice); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request Body"})
+		return
+	}
+
+	// call the service layer here
+	m, err := s.sci.RegisterDevice(newDevice.SNo, newDevice.FirmwareVersion)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidFV) || errors.Is(err, service.ErrInvalidSno) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid inputs"})
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("%s", err)})
+	}
+	c.JSON(http.StatusCreated, m)
+}
+
+// HandleUpdateMeshRequest will handle the HTTP request to flip the mesh status of device with given serial number
+func (s *ServiceContractInstance) HandleUpdateMeshRequest(c *gin.Context) {
+	// instantiate a new object to bind the HTTP JSON to
+	var device UpdateMeshRequest
+
+	// Gin will handle decoding and error response
+	if err := c.ShouldBindJSON(&device); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request Body"})
+		return
+	}
+
+	// call the service layer here
+	m, err := s.sci.UpdateMeshStatus(device.SNo)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidSno) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid serial number"})
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("%s", err)})
+	}
+	c.JSON(http.StatusOK, m)
+}
+
+// HandleDeviceRetrieval will handle the HTTP request to retrieve information on a particular device
+func (s *ServiceContractInstance) HandleDeviceRetrieval(c *gin.Context) {
+	// instantiate a new object to bind the HTTP JSON to
+	var device RetrieveDeviceRequest
+
+	// Gin will handle decoding and error response
+	if err := c.ShouldBindJSON(&device); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request Body"})
+		return
+	}
+
+	// call the service layer here
+	m, err := s.sci.RetrieveById(device.SNo)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidSno) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid serial number"})
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("%s", err)})
+	}
+	c.JSON(http.StatusOK, m)
+}
